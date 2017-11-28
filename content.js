5056bfaa9bbabea857a32442a5b0b48f62540733
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2017-11-28 16:43:08 +0700

    feat(template): add template

--- content.js
+++ content.js
@@ -1,17 +1,40 @@
-// chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.method == "getSelection")
-//     sendResponse({data: window.getSelection().toString()})
-//   else
-//     sendResponse({})
-// })
+const createTemplate = ({ letter, pinyin, sinoViet, strokeImage, meaning }) => {
+  console.log(meaning)
+  const template = `
+    <div class="pexea12-hv-container">
+      <p class="pexea12-hv-letter">
+        ${ letter }
+      </p>
+      <p class="pexea12-hv-pronunciation">
+        <span>Pinyin:</span> 
+        ${ pinyin.join(', ') }
+      </p>
+      <p class="pexea12-hv-pronunciation">
+        <span>Hán Việt:</span>
+        ${ sinoViet.join(', ') }
+      </p>
+      <p class="pexea12-hv-meaning">
+        ${ meaning.replace(/\n/g, '<br/>') }
+      </p>
+      <p class="pexea12-hv-strokes">
+          <img src="${ strokeImage }">
+      </p>
+    </div>
+  `
+
+  return template
+}
+
 
 const textProcess = () => {
   const text = window.getSelection().toString().replace(/[\s\n]+/g, '')
 
   if (text.length === 1 && text[0] >= '\u3400' && text[0] <= '\u9FBF') {
     chrome.runtime.sendMessage({ text }, (res) => {
-      console.log('content')
-      console.log(res)
+      const popup = createTemplate(res)
+
+      document.body.innerHTML += popup
+      console.log(document.body.innerHTML)
     })
   }
 }
