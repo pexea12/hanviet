ef3ae0d759d1e1f793d3e3e9cd24f4dcefa0410b
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2017-11-28 10:49:25 +0700

    feat(background): add fetch pinyin

--- background.js
+++ background.js
@@ -1,23 +1,41 @@
-// chrome.tabs.query({
-//     active: true, 
-//     windowId: chrome.windows.WINDOW_ID_CLIENT
-// }, (tab) => {
+const fetchText = (text) => {
+  const getPinyin = fetch(`http://hvdic.thivien.net/wpy/${ text }`)
+    .then((res) => {
+      return res.text()
+    })
+    .then((data) => {
+      console.log(data)
+    })
 
-// })
+  const getSino = fetch(`http://hvdic.thivien.net/whv/${ text }`) 
+
+  // return Promise.all([ getPinyin, getSino ])
+  //   .then((res) => {
+  //   })
+  //   .then((data) => {
+  //     const pinyinRes = document.createElement(data[0])
+  //     const sinoRes = document.createElement(data[1])
+
+  //     const pinyin = Array.from(
+  //         pinyinRes.querySelector('.info')
+  //           .getElementsByClassName('hvres-goto-link')
+  //       )
+  //       .map(span => span.innerHTML)
+
+  //     const strokeImage = pinyinRes
+  //       .querySelector('.hvres-animation')
+  //       .querySelector('.lazy')
+  //       .src
+
+
+  //   })
+}
+
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message)
+    const { text } = message
+
+    fetchText(text)
 
     sendResponse({ message: 'hello world from background' })
-
-    // chrome.tabs.query({ 
-    //     active: true,
-    //     currentWindow: true,
-    // }, (tabs) => {
-    //     const tab = tabs[0]
-    //     chrome.tabs.sendMessage(tab.id, { message: 'hello world from background' }, (res) => {
-    //         console.log('background')
-    //         console.log(res)
-    //     })
-    // })
 })
