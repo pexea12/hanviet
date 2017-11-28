f22f816e0d0d6e095d7cbd043edd327ee18dc01d
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2017-11-28 16:57:57 +0700

    style(container): change height of meaning

--- content.js
+++ content.js
@@ -1,23 +1,18 @@
-const createTemplate = ({ letter, pinyin, sinoViet, strokeImage, meaning }) => {
-  console.log(meaning)
+const createTemplate = () => {
   const template = `
     <div class="pexea12-hv-container">
-      <p class="pexea12-hv-letter">
-        ${ letter }
-      </p>
+      <p class="pexea12-hv-letter"></p>
       <p class="pexea12-hv-pronunciation">
         <span>Pinyin:</span> 
-        ${ pinyin.join(', ') }
+        <span class="pexea12-hv-pinyin"></span>
       </p>
       <p class="pexea12-hv-pronunciation">
         <span>Hán Việt:</span>
-        ${ sinoViet.join(', ') }
+        <span class="pexea12-hv-sinoviet"></span>
       </p>
-      <p class="pexea12-hv-meaning">
-        ${ meaning.replace(/\n/g, '<br/>') }
-      </p>
+      <p class="pexea12-hv-meaning"></p>
       <p class="pexea12-hv-strokes">
-          <img src="${ strokeImage }">
+        <img src="">
       </p>
     </div>
   `
@@ -26,15 +21,30 @@ const createTemplate = ({ letter, pinyin, sinoViet, strokeImage, meaning }) => {
 }
 
 
+const setTemplate = (container, { letter, pinyin, sinoViet, strokeImage, meaning }) => {
+  container.querySelector('.pexea12-hv-letter').innerText = letter 
+  container.querySelector('.pexea12-hv-pinyin').innerText = pinyin.join(', ')
+  container.querySelector('.pexea12-hv-sinoviet').innerText = sinoViet.join(', ')
+  container.querySelector('.pexea12-hv-meaning').innerHTML = meaning.replace(/\n/g, '<br/>')
+  container.querySelector('.pexea12-hv-strokes img').src = strokeImage
+}
+
+
 const textProcess = () => {
   const text = window.getSelection().toString().replace(/[\s\n]+/g, '')
 
   if (text.length === 1 && text[0] >= '\u3400' && text[0] <= '\u9FBF') {
     chrome.runtime.sendMessage({ text }, (res) => {
-      const popup = createTemplate(res)
+      let container = document.querySelector('.pexea12-hv-container')
 
-      document.body.innerHTML += popup
-      console.log(document.body.innerHTML)
+      if (!container) {
+        const popup = createTemplate(res)
+        document.body.innerHTML += popup
+        container = document.querySelector('.pexea12-hv-container')
+      }
+      
+      setTemplate(container, res)
+
     })
   }
 }
