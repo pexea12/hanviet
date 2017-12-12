13a80a4bdf0606e6d41189151412d5bcecc86f9b
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2017-12-12 17:42:10 +0700

    feat(popup): add loading symbol

--- content.js
+++ content.js
@@ -1,19 +1,24 @@
 const createTemplate = () => {
   const template = `
     <div class="pexea12-hv-container">
-      <p class="pexea12-hv-letter"></p>
-      <p class="pexea12-hv-pronunciation">
-        <span>Pinyin:</span> 
-        <span class="pexea12-hv-pinyin"></span>
-      </p>
-      <p class="pexea12-hv-pronunciation">
-        <span>Hán Việt:</span>
-        <span class="pexea12-hv-sinoviet"></span>
-      </p>
-      <p class="pexea12-hv-meaning"></p>
-      <p class="pexea12-hv-strokes">
-        <img src="">
-      </p>
+      <div class="pexea12-hv-loading">
+        <div></div>
+      </div>
+      <div class="pexea12-hv-content">
+        <p class="pexea12-hv-letter"></p>
+        <p class="pexea12-hv-pronunciation">
+          <span>Pinyin:</span> 
+          <span class="pexea12-hv-pinyin"></span>
+        </p>
+        <p class="pexea12-hv-pronunciation">
+          <span>Hán Việt:</span>
+          <span class="pexea12-hv-sinoviet"></span>
+        </p>
+        <p class="pexea12-hv-meaning"></p>
+        <p class="pexea12-hv-strokes">
+          <img src="">
+        </p>
+      </div>
       <div class="pexea12-hv-cancel">
         <a>Close</a>
       </div>
@@ -24,31 +29,47 @@ const createTemplate = () => {
 }
 
 
-const setTemplate = (container, { letter, pinyin, sinoViet, strokeImage, meaning }) => {
+const popup = createTemplate()
+document.body.innerHTML += popup
+
+const container = document.querySelector('.pexea12-hv-container')
+const button = container.querySelector('.pexea12-hv-cancel')
+const loading = container.querySelector('.pexea12-hv-loading')
+const content = container.querySelector('.pexea12-hv-content')
+
+const setTemplate = ({ letter, pinyin, sinoViet, strokeImage, meaning }) => {
   container.querySelector('.pexea12-hv-letter').innerText = letter 
   container.querySelector('.pexea12-hv-pinyin').innerText = pinyin.join(', ')
   container.querySelector('.pexea12-hv-sinoviet').innerText = sinoViet.join(', ')
   container.querySelector('.pexea12-hv-meaning').innerHTML = meaning.replace(/\n/g, '<br/>')
   container.querySelector('.pexea12-hv-strokes img').src = strokeImage
-
-  container.style.display = 'block'
 }
 
 
-const popup = createTemplate()
-document.body.innerHTML += popup
-
-const container = document.querySelector('.pexea12-hv-container')
+const toggleLoading = (on = true)  => {
+  if (on) {
+    loading.style.display = 'block'
+    content.style.display = 'none'
+  } else {
+    loading.style.display = 'none'
+    content.style.display = 'block'
+  }
+}
 
 document.addEventListener('mouseup', () => {
   const text = window.getSelection().toString().replace(/[\s\n]+/g, '')
 
   if (text.length === 1 && text[0] >= '\u3400' && text[0] <= '\u9FBF') {
-    chrome.runtime.sendMessage({ text }, res => setTemplate(container, res))
+    container.style.display = 'block'
+
+    toggleLoading()
+    chrome.runtime.sendMessage({ text }, (res) => {
+      toggleLoading(false)
+      setTemplate(res)
+    })
   }
 })
 
-const button = container.querySelector('.pexea12-hv-cancel')
 button.addEventListener('click', () => {
   container.style.display = 'none'
 })
